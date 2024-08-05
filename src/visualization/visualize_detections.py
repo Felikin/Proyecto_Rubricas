386cdd7c9d85e5d3be7b9f4ebe32a7bc5dde576a
Felipe González <felipe.gonzalez@unir.net>
2024-08-05 14:57:52 -0500

    Added slides count feature, Updated typehint and library description, updated function names

--- src/visualization/visualize_detections.py
+++ src/visualization/visualize_detections.py
@@ -1,5 +1,21 @@
-def print_detection_result(detections):
+def print_detection_result(detections: list) -> None:
+    """
+    Suma el total de veces que fue detectado el rostro del profesor en el vídeo.
+
+    Args:
+    detections (list): Lista de detecciones realizadas
+    """
     sum_persons = sum([1 for detection in detections if detection == "person"])
 
     if sum_persons >= len(detections) * 0.6:
         print("El profesor encendió la cámara")
+
+
+def print_slides_count(slides: int) -> None:
+    """
+    Imprime el número de slides contadas.
+
+    Args:
+    slides (int): Número de slides contadas.
+    """
+    print(f"el número de diapositivas aproximado fue de {slides}") 
