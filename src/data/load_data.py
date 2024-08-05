9304b374888f85cab767245b6cdceee496cec37a
Felipe González <felipe.gonzalez@unir.net>
2024-08-05 14:56:51 -0500

    Added load yolo model function and updated Typehint and libraries

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,14 +1,32 @@
 import os
 import cv2
+from ultralytics import YOLO
 
-# def load_video(video_path):
-#     if os.path.isfile(video_path):
-#         return video_path
-#     else:
-#         raise ValueError(f"Error opening video file: {video_path}")
 
-def load_video(video_path):
+def load_video(video_path: str) -> cv2.VideoCapture:
+    """
+    Carga un video desde la ruta especificada.
+    
+    Args:
+    video_path (str): Ruta al video.
+
+    Returns:
+    video (cv2.VideoCapture): video abierto con opencv.
+    """
     video = cv2.VideoCapture(video_path)
     if not video.isOpened():
         raise ValueError(f"Error opening video file: {video_path}")
     return video
+
+
+def load_yolo_model(model_path: str) -> YOLO:
+    """
+    Carga un modelo YOLO preentrenado desde la ruta especificada.
+    
+    Args:
+    model_path (str): Ruta al modelo YOLO.
+
+    Returns:
+    YOLO: Modelo YOLO cargado.
+    """
+    return YOLO(model_path)
