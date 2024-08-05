ea1ee3799684d026a6d54d62f6cf5f19649b13ba
Felipe González <felipe.gonzalez@unir.net>
2024-08-05 14:56:03 -0500

    Updated typehint and functions names

--- scripts/face_detection.py
+++ scripts/face_detection.py
@@ -3,16 +3,17 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.data.load_data import load_video
-from src.models.yolo_model import load_yolo_model, frames_det
-from src.visualization.visualize_detections import print_detection_result
+from src.models.video_model import process_video
+from src.visualization.visualize_detections import print_detection_result, print_slides_count
 
-def main(video_path):
-    video = load_video(video_path)
-    model = load_yolo_model("models/yolov8n.pt")
-    detections = frames_det(model=model, video=video)
+
+def main(video_path: str, model_path: str) -> None:
+    detections, slides = process_video(model_path=model_path, video_path=video_path)
     print_detection_result(detections)
+    print_slides_count(slides)
+
 
 if __name__ == "__main__":
     video_path = 'data/raw/Juan Sebastian Lozano.mp4'
-    main(video_path)
+    model_path = 'models/yolov8n.pt'
+    main(video_path, model_path)
