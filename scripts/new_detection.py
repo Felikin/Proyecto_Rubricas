f69e85eec687e58d28d3f5fa9e6eab55ed157dcf
Felipe González <felipe.gonzalez@unir.net>
2024-08-30 11:28:10 -0500

    Actualizado modulo para procesar video

--- scripts/new_detection.py
+++ scripts/new_detection.py
@@ -3,50 +3,16 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import time
-import cv2
-import base64
-import matplotlib.pyplot as plt
-from src.data.load_data import load_video
-from src.data.preprocess import encode_image_to_base64
-from src.data.load_data import initialize_gpt_client
-from src.models.gpt_model import send_frame_to_gpt
+from src.models.gpt_model import process_video
 from src.visualization.visualize_detections import print_gpt
-from src.utils.video_utils import process_gpt_outputs
-from tqdm import tqdm
-
 
 
 def main(video_path, interval_seconds):
-    
 
-    # Inicializar cliente GPT
-    client = initialize_gpt_client()
-
-    # Cargar el video
-    video = load_video(video_path)
-    fps = int(video.get(cv2.CAP_PROP_FPS))
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    frames_to_capture = range(0, total_frames, interval_seconds * fps)
-
-    # Procesar cada frame seleccionado
-    model_output = []
-    for frame_index in tqdm(frames_to_capture, desc = "Analizando video"):
-        video.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        ret, frame = video.read()
-        if not ret:
-            break
-        base64_image = encode_image_to_base64(frame)
-        generated_text = send_frame_to_gpt(base64_image, client)
-        model_output.append(generated_text)
-        with open(f"data/generated/{frame_index}.png", "wb") as fh:
-             fh.write(base64.decodebytes(bytes(base64_image, "utf-8")))
-    video.release()
-    
-    results = process_gpt_outputs(model_output)
+    results = process_video(video_path, interval_seconds)
     print_gpt(results)
 
 if __name__ == "__main__":
         video_path = "data/raw/Juan Sebastian Lozano.mp4"
-        interval_seconds = 60
+        interval_seconds = 1800
         main(video_path, interval_seconds)
--- src/models/gpt_model.py
+++ src/models/gpt_model.py
@@ -1,11 +1,40 @@
-import os
-import json
-from openai import OpenAI
+import cv2
+import base64
+from typing import List, Dict
+from tqdm import tqdm
 from dotenv import load_dotenv
+from src.data.load_data import load_video, initialize_gpt_client
+from src.data.preprocess import encode_image_to_base64
+from src.utils.video_utils import process_gpt_outputs
 load_dotenv()
 
+def process_video(video_path: str, interval_seconds: int) -> Dict:
+    # Cargar el video
+    video = load_video(video_path)
+    fps = int(video.get(cv2.CAP_PROP_FPS))
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    frames_to_capture = range(0, total_frames, interval_seconds * fps)
+    # Procesar cada frame seleccionado
+    model_output = []
+    for frame_index in tqdm(frames_to_capture, desc = "Analizando video"):
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+        ret, frame = video.read()
+        if not ret:
+            break
+        base64_image = encode_image_to_base64(frame)
+        generated_text = send_frame_to_gpt(base64_image)
+        print(generated_text)
+        model_output.append(generated_text)
+        with open(f"data/generated/{frame_index}.png", "wb") as fh:
+             fh.write(base64.decodebytes(bytes(base64_image, "utf-8")))
+    video.release()
+    results = process_gpt_outputs(model_output)
+    cv2.destroyAllWindows()
 
-def send_frame_to_gpt(frame, client):
+    return results
+
+
+def send_frame_to_gpt(frame):
     """Envía un frame de video a GPT para análisis y obtiene el resultado.
 
     Args:
@@ -15,6 +44,8 @@ def send_frame_to_gpt(frame, client):
     Returns:
         dict: Resultado del análisis del frame.
     """
+    client = initialize_gpt_client()
+
     prompt_message = """
         Por favor, analiza la siguiente imagen codificada en base64 proveniente de un video de una clase y 
         1. Presencia del Profesor: Indica si el profesor aparece en cámara en este frame. Devuelve true si aparece, de lo contrario, false.\n
