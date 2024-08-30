3931b3577c91d1ee82dc19e1514e39cc4ebec7a7
Felipe González <felipe.gonzalez@unir.net>
2024-08-30 09:57:35 -0500

    Actualizado conteo de diapositivas y detección de gráficos.

--- src/utils/video_utils.py
+++ src/utils/video_utils.py
@@ -1,24 +1,34 @@
 from collections import deque
 from statistics import mode
 import json
+import re
+from tqdm import tqdm
 
 def process_gpt_outputs(gpt_outputs):
     professor_count = 0
     slide_count = 0
+    graficos = 0
     text_quantities = []
 
-    for output in gpt_outputs:
-        output_dict = json.loads(output)
-        if output_dict["Camara"] == "True":
-            professor_count += 1
-        if output_dict["Slide"]:
-            slide_count += 1
-        text_quantities.append(output_dict["Contenido"]["Cantidad de texto"])
+    for output in tqdm(gpt_outputs, desc="Calculando resultados"):
+        if output is not None: 
+            json_str = re.sub("```|json", "", output)
+            output_dict = json.loads(json_str)
+            if output_dict["Camara"] == "True":
+                professor_count += 1
+            if output_dict["Slide"]:
+                slide_count += 1
+            if output_dict["Contenido"]["graficos"]:
+                graficos += 1
+            text_quantities.append(output_dict["Contenido"]["Cantidad de texto"])
+        else:
+            pass
     
     most_common_text_quantity = mode(text_quantities)
     
     return {
-        "Profesor_en_Cámara": professor_count > 0,
-        "Diapositivas_presentes": slide_count > 0,
-        "Moda_de_Texto": most_common_text_quantity
+        "Profesor_en_Cámara": professor_count > len(gpt_outputs)/2,
+        "Diapositivas_presentes": slide_count,
+        "Moda_de_Texto": most_common_text_quantity,
+        "Gráficos": graficos > len(gpt_outputs)/3
     }
